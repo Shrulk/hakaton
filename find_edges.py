a3d64c5ec82cb4f32332cadabd74a52e69554b39
Vladislav <novikov.vs@phystech.edu>
2024-04-09 22:11:56 +0300

    added gaussian blurring image for avoid water wave's edges + using cv2 method createLineSegmentDetector

--- find_edges.py
+++ find_edges.py
@@ -8,22 +8,29 @@ image = cv2.imread('frames/frame_1875.png')[:, :, 0]
 plt.imshow(image, cmap='gray')
 plt.show()
 
+blur_image = cv2.GaussianBlur(image, (9, 9), sigmaX=5, sigmaY=5)
+plt.imshow(blur_image, cmap='gray')
+plt.show()
 
-lower_threshold = 145
-high_threshold = 150
-L2gradients = True
-edges = cv2.Canny(image, lower_threshold, high_threshold, L2gradient=L2gradients)
-print(np.where(edges == 255))
+# lower_threshold = 145
+# high_threshold = 150
+# L2gradients = True
+# edges = cv2.Canny(blur_image, lower_threshold, high_threshold, L2gradient=L2gradients)
+# print(np.where(edges == 255))
+#
+# lines_list =[]
+# lines = cv2.HoughLinesP(
+#             edges,   # Input edge image
+#             1,   # Distance resolution in pixels
+#             np.pi/180,   # Angle resolution in radians
+#             threshold=10,   # Min number of votes for valid line
+#             minLineLength=20,   # Min allowed length of line
+#             maxLineGap=10   # Max allowed gap between line for joining them
+#             )
 
-lines_list =[]
-lines = cv2.HoughLinesP(
-            edges,   # Input edge image
-            1,   # Distance resolution in pixels
-            np.pi/180,   # Angle resolution in radians
-            threshold=100,   # Min number of votes for valid line
-            minLineLength=60,   # Min allowed length of line
-            maxLineGap=10   # Max allowed gap between line for joining them
-            )
+lsd = cv2.createLineSegmentDetector()
+lines = lsd.detect(blur_image)[0]
+
 for points in lines:
     # Extracted points nested in the list
     x1, y1, x2, y2 = points[0]
@@ -31,7 +38,7 @@ for points in lines:
     # On the original image
     plt.plot([x1, x2], [y1, y2], color='red')
     # Maintain a simples lookup list for points
-    lines_list.append([(x1, y1), (x2, y2)])
+    # lines_list.append([(x1, y1), (x2, y2)])
 
 plt.imshow(image, cmap='gray')
 # plt.scatter(np.where(edges == 255)[1], np.where(edges == 255)[0], s=0.1, color='red')
