3194a7155a036dffb28dd345064340b217e470fc
Shrulk <58909805+Shrulk@users.noreply.github.com>
2024-04-13 20:02:42 +0300

    Взял код из статьи и начал переделывать настройку датасета под нашу задачу. Сгенерировал все xml-описания кадров в marked_frames/xml_s/ и соответствующие им BGR(?) картинки в marked_frames/colored_frames/

--- frame-prepare.py
+++ frame-prepare.py
@@ -2,34 +2,43 @@ import cv2
 
 capture = cv2.VideoCapture('CNTRL.mp4')
 frame_shift = 10
-frameNr = 25 * 6    # Пропускаем первые 6 секунд видео (там стропы ещё не натянуты)
+frameNr = 0    # Пропускаем первые 6 секунд видео (там стропы ещё не натянуты)
 
 def get_frame_name(n):
     fr_num = str(n).zfill(4)
     # return f'frames/frame_{fr_num}.png'
-    return f'frames_to_be_marked/frame_{fr_num}.png'
+    return f'D:/Education/10_sem/ml/hakaton/marked_frames/colored_frames/frame_{fr_num}.png'
 
 # check work dir
 # import os
 # print(os.path.abspath(os.curdir))
+import os
 
-while (True):
+files = os.listdir("marked_frames")
+nums = []
+for file in files:
+    if file in ["xml_s", "colored_frames"]: continue
+    nums.append(int(file[-9:-5]))
+
+# while (True):
     # Пропускаем кадры от 1:40 до 2:36 (от момента отцепления первого груза до захвата второго)
     # И пропускаем кадры после 4:41, т.к. там работы уже закончены
-    if (frameNr > 25 * 100 and frameNr < 25 * (2*60 + 36)) or (frameNr > 25 * (4*60 +41)):
-        frameNr = frameNr + frame_shift
-        break
-    if frameNr > 8000: break
+    # if (frameNr > 25 * 100 and frameNr < 25 * (2*60 + 36)) or (frameNr > 25 * (4*60 +41)):
+    #     frameNr = frameNr + frame_shift
+    #     continue
+    # if frameNr > 8000: break
 
+for frameNr in nums:
     capture.set(cv2.CAP_PROP_POS_FRAMES, frameNr)
     success, frame = capture.read()
     if success:
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(get_frame_name(frameNr), gray_frame)
+        # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(get_frame_name(frameNr), frame)
 
-    else:
-        break
-    # frame += frame_shift
-    frameNr = frameNr + frame_shift
+    # else:
+    #     break
+    # # frame += frame_shift
+    # frameNr = frameNr + frame_shift
  
 capture.release()
