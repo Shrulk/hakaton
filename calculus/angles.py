0806f208a514950f1368fb5cc09dc7caa1962ac7
Shrulk <58909805+Shrulk@users.noreply.github.com>
2024-04-24 22:10:32 +0300

    Добавлен main.py с основным циклом

--- calculus/angles.py
+++ calculus/angles.py
@@ -1,30 +1,32 @@
 import numpy as np
 
+
 class Point2D:
-    def __init__(self, x=0., y=0.) -> None:
+    def __init__(self, x=0.0, y=0.0) -> None:
         self.x = x
         self.y = y
-    
+
+
 class Line:
-    def __init__(self, x1=0., y1=0., x2=1., y2=1.) -> None:
+    def __init__(self, x1=0.0, y1=0.0, x2=1.0, y2=1.0) -> None:
         self.p1 = Point2D(x1, y1)
         self.p2 = Point2D(x2, y2)
 
-    def k (self) -> float:
-        '''Угол наклона вектора на плоскости'''
+    def k(self) -> float:
+        """Угол наклона вектора на плоскости"""
         return (self.p2.y - self.p1.y) / (self.p2.x - self.p1.x)
 
-    def b (self) -> float:
-        '''Сдвиг вдоль горизонтальной оси'''
-        return(self.p2.x * self.p1.y - self.p1.x * self.p2.y) / (self.p2.x - self.p1.x)
+    def b(self) -> float:
+        """Сдвиг вдоль горизонтальной оси"""
+        return (self.p2.x * self.p1.y - self.p1.x * self.p2.y) / (self.p2.x - self.p1.x)
 
-    def angle(self, line, type='rad') -> float:
-        '''Угол между прямыми.
+    def angle(self, line, type="rad") -> float:
+        """Угол между прямыми.
         - Парметр type: если type = "rad" (по умолчанию), то результат функции возвращается в радианах,
-        если type = "grad", то результат будет переведён в градусы'''
+        если type = "grad", то результат будет переведён в градусы"""
         k1 = self.k()
         k2 = line.k()
-        znam = (1. + k2 * k1)
+        znam = 1.0 + k2 * k1
         if znam == 0:
             if type == "rad":
                 return np.pi
@@ -38,13 +40,13 @@ class Line:
             return np.rad2deg(rad)
 
     def equation(self, order=10) -> None:
-        '''Выводит в консоль уравнение прямой в формате y = kx + b
-        - параметр order: число знаков после запятой при выводе'''
-        print(f'y = {round(self.k(), order)} x + {round(self.b(), order)}')
+        """Выводит в консоль уравнение прямой в формате y = kx + b
+        - параметр order: число знаков после запятой при выводе"""
+        print(f"y = {round(self.k(), order)} x + {round(self.b(), order)}")
 
 
-def angles(lines, type='rad'):
-    '''
+def angles(lines, type="rad"):
+    """
     Возвращает матрицу попарных углов между прямыми из входного массива.
 
     - Пример:\\
@@ -60,19 +62,19 @@ def angles(lines, type='rad'):
      [ 5.96143475  1.24904577  0.         -0.98279372 -1.38544838]\\
      [ 0.66104317  5.37343215  0.98279372  0.         -0.40265465]\\
      [ 1.06369782  5.7760868   1.38544838  0.40265465  0.        ]]\\
-    '''
+    """
     res = np.zeros((len(lines), len(lines)))
     for i in range(len(lines)):
         for j in range(len(lines)):
             if i == j:
                 continue
-            if (i > j):
+            if i > j:
                 continue
             line1 = lines[i]
             line2 = lines[j]
             res[i][j] = line1.angle(line2, type)
-            if type=="rad":
-                res[j][i] = np.mod(2*np.pi- res[i][j], 2*np.pi)
+            if type == "rad":
+                res[j][i] = np.mod(2 * np.pi - res[i][j], 2 * np.pi)
             elif type == "grad":
-                res[j][i] = np.mod(180. - res[i][j], 180)
+                res[j][i] = np.mod(180.0 - res[i][j], 180)
     return res
---
+++ main.py
@@ -0,0 +1,130 @@
+import numpy as np
+import cv2
+import torch
+import glob as glob
+import os
+import json
+from our_nn.model import create_model
+import matplotlib.pyplot as plt
+import time
+from calculus.angles import Line, angles
+
+
+CLASSES = ["background", "sling", "box"]
+detection_threshold = 0.8
+
+# выбор процессора
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# загрузка обученной модели с весами
+model = create_model(num_classes=3).to(device)
+model.load_state_dict(torch.load("outputs/model_150_END.pth", map_location=device))
+model.eval()
+
+def table(angles):
+    pass
+
+
+
+def get_sling_ends(inp):
+    return inp[0], inp[1]
+
+
+def process_video():
+    # Читаем файл с видео
+    cap = cv2.VideoCapture(file_name)
+
+    # Получаем высоту, ширину и количество кадров в видео
+    width, height, frame_count = (
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
+    )
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    print("Количество кадров = {}".format(frame_count))
+    print("Ширина = {}, Длина = {}".format(width, height))
+    print("FPS = {}".format(fps))
+
+    # Определяем кодек и создаем объект VideoWriter
+    fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+    out = cv2.VideoWriter()
+    output_file_name = "output.mp4"
+    out.open(output_file_name, fourcc, fps, (width, height), True)
+    i = 0
+    outputs = None
+    try:
+        while cap.isOpened():
+            i += 1
+            ret, image = cap.read()
+            if not ret:
+                break
+
+            orig_image = image.copy()
+            if i % (fps // 3) == 0:
+                # BGR to RGB
+                image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB).astype(np.float32)
+                # нормируем значение пикселей между 0 и 1
+                image /= 255.0
+                image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+                image = torch.tensor(image, dtype=torch.float).cuda()
+                # повышение размерности для батча
+                image = torch.unsqueeze(image, 0)
+                with torch.no_grad():
+                    outputs = model(image)
+                outputs = [{k: v.to("cpu") for k, v in t.items()} for t in outputs]
+                # Выполняем распознавание
+            if outputs is not None and len(outputs[0]["boxes"]) != 0:
+                boxes = outputs[0]["boxes"].data.numpy()
+                scores = outputs[0]["scores"].data.numpy()
+
+                # фильтруем результат по detection_threshold
+                boxes = boxes[scores >= detection_threshold].astype(np.int32)
+                draw_boxes = boxes.copy()
+                # получаем название найденных классов
+                pred_classes = [CLASSES[i] for i in outputs[0]["labels"].cpu().numpy()]
+                for j, box in enumerate(draw_boxes):
+                    if pred_classes[j] == "box":  # box:
+                        cv2.rectangle(
+                            orig_image,
+                            (int(box[0]), int(box[1])),
+                            (int(box[2]), int(box[3])),
+                            (0, 255, 0),
+                            2,
+                        )
+                        cv2.putText(
+                            orig_image,
+                            "cargo",
+                            (int(box[0]), int(box[3] - 5)),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (255, 0, 255),
+                            1,
+                            lineType=cv2.LINE_AA,
+                        )
+                    elif pred_classes[j] == "sling":
+                        p1, p2 = get_sling_ends(
+                            [  # [(x1,y1), (x2,y2)]
+                                (int(box[0]), int(box[1])),
+                                (int(box[2]), int(box[3])),
+                            ]
+                        )
+                        cv2.line(orig_image, p1, p2, (0, 255, 0), 3)
+            # Рисуем рамку
+            out.write(orig_image)
+    except:
+        # Высвобождаем ресурсы
+        cap.release()
+        out.release()
+
+    # Высвобождаем ресурсы
+    cap.release()
+    out.release()
+
+
+file_name = "cutted.mp4"
+# file_name = "CNTRL.mp4"
+output_file_name = "output.mp4"
+start_time = time.time()
+process_video()
+end_time = time.time()
+total_processing_time = end_time - start_time
+print("Время: {}".format(total_processing_time))
