798c477ce4ef8d3fa6823a65e975ae786de0340c
novikmeister <75265247+novikmeister@users.noreply.github.com>
2024-04-24 21:10:03 +0300

    Add files via upload

--- cut_frames.py
+++ cut_frames.py
@@ -1,27 +1,20 @@
 import numpy as np
-from typing import Tuple, Union
 import cv2
-import json
 
 def cut_image(image_source: str,
-              boxes_source: str) -> Tuple[np.ndarray, Union[int, None], Union[int, None]]:
+              box_coords: list) -> np.ndarray:
     """
-    входные данные:
+    Входные данные:
     image_source: путь к файлу с изображением
-    boxes_source: путь к файлу .json с боксами для одного изображения
+    box_coords: лист из координат бокса 
     
-    выходные данные:
-    1) обрезанное изображение от начала контейнера до верха изображения
-    2) значения по координатам x и y для обратного перехода к координатам полного изображения из обрезанного
+    Выходные данные:
+    1) обрезанное изображение с областью со стропой
     """
     image = cv2.imread(image_source)
-    with open(boxes_source) as json_file:
-        result = json.load(json_file)
-    if result['labels'] == None:
-        return image, None, None
+    if box_coords == None:
+        return image
     else:
-        box_index = result['labels'].index('box')
-        box = result['boxes'][box_index]
-        x1, y1, x2, y2 = box
-        return image[:y2, x1:x2], x1, y1
+        x1, y1, x2, y2 = box_coords
+        return image[y1:y2, x1:x2]
     
