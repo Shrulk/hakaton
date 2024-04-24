59769f99a8568115f578b9d49ed7354be40a370d
Мирпапур <46839139+mrpepperbest@users.noreply.github.com>
2024-04-25 00:43:35 +0300

    Написана функция построения диагональной матрицы, изменен расчет метрик диагонали.

--- cv_module/line_detection.py
+++ cv_module/line_detection.py
@@ -4,79 +4,53 @@ import cv2 as cv
 from cv_module import cut_frames
 
 
-def is_in_image(
-    index: typing.Union[np.ndarray, list, tuple],
-    shape: typing.Union[np.ndarray, list, tuple],
-    *args,
-    **kwargs
-):
+def is_in_image(index: typing.Union[np.ndarray, list, tuple],
+                shape: typing.Union[np.ndarray, list, tuple],
+                *args, **kwargs):
     x = np.max([0, np.min([index[0], shape[0] - 1])])
     y = np.max([0, np.min([index[1], shape[1] - 1])])
     return x, y
 
 
-def diagonal_elements_extraction(
-    image: typing.Union[np.ndarray, list], gap: int, *args, **kwargs
-) -> typing.Tuple[list, list]:
-    diagonal_distribution_left = []
-    diagonal_distribution_right = []
+def rectangular_eye_crafting(shape, shift=0):
+    matrix = np.zeros(shape)
+    k = shape[1]/shape[0]
+    b = shift
+    if k >= 1:
+        for i in range(shape[0]):
+            index = np.min([int(k*i+b), shape[1]-1])
+            index_2 = np.min([int(k*i+b)+int(np.round(k, 0)), shape[1]-1])
+            matrix[i][index:index_2] = 1
+    else:
+        matrix = np.zeros((shape[1], shape[0]))
+        k = shape[0]/shape[1]
+        b = shift
+        for i in range(shape[1]):
+            index = np.min([int(k*i+b), shape[0]-1])
+            index_2 = np.min([int(k*i+b)+int(np.round(k, 0)), shape[0]-1])
+            matrix[i][index:index_2] = 1
+        matrix = matrix.T
+    return np.array(matrix != 0, dtype=int)
+
+
+def diagonal_elements_extraction(image: typing.Union[np.ndarray, list], gap: int, *args, **kwargs) -> typing.Tuple[list, list]:
     image_shape = np.shape(image)
-    i_s_left = np.arange(0, image_shape[0], 1)
-    j_s_left = np.arange(0, image_shape[1], 1)
+    eye_l = rectangular_eye_crafting(image_shape)
+    for i in range(gap):
+        eye_l = eye_l + rectangular_eye_crafting(image_shape, i) + rectangular_eye_crafting(image_shape, -i)
+    eye_l = np.array(eye_l != 0, dtype=int)
+    eye_r = np.flip(eye_l, axis=1)
+    diagonal_distribution_left = image * eye_l
 
-    i_s_right = np.arange(image_shape[0] - 1, -1, -1)
-    j_s_right = np.arange(0, image_shape[1], 1)
-
-    indices_left = np.array(list(zip(i_s_left, j_s_left)))
-    indices_right = np.array(list(zip(i_s_right, j_s_right)))
-
-    for number in range(gap):
-        indices_left = np.concatenate(
-            (
-                np.copy(indices_left),
-                np.copy(indices_left) + [0, number],
-                np.copy(indices_left) + [number, 0],
-                np.copy(indices_left) - [0, number],
-                np.copy(indices_left) - [number, 0],
-            )
-        )
-        indices_right = np.concatenate(
-            (
-                np.copy(indices_right),
-                np.copy(indices_right) + [0, number],
-                np.copy(indices_right) + [number, 0],
-                np.copy(indices_right) - [0, number],
-                np.copy(indices_right) - [number, 0],
-            )
-        )
-    indices_left = set(map(lambda x: tuple(x), indices_left))
-    indices_right = set(map(lambda x: tuple(x), indices_right))
-    list(
-        map(
-            lambda x: diagonal_distribution_left.append(
-                image[is_in_image(x, image_shape)]
-            ),
-            indices_left,
-        )
-    )
-    list(
-        map(
-            lambda x: diagonal_distribution_right.append(
-                image[is_in_image(x, image_shape)]
-            ),
-            indices_right,
-        )
-    )
+    diagonal_distribution_right = image * eye_r
     return diagonal_distribution_left, diagonal_distribution_right
 
 
-def diagonal_max_mean_definition(
-    box: typing.Union[np.ndarray, list], gap: int, *args, **kwargs
-) -> bool:
-    diagonal_distribution_left, diagonal_distribution_right = (
-        diagonal_elements_extraction(image=box, gap=gap)
-    )
-    if np.median(diagonal_distribution_left) >= np.median(diagonal_distribution_right):
+def diagonal_max_mean_definition(box: typing.Union[np.ndarray, list], gap: int, *args, **kwargs) -> bool:
+    diagonal_distribution_left, diagonal_distribution_right = diagonal_elements_extraction(image=box,
+                                                                                           gap=gap)
+    print(np.sum(diagonal_distribution_left), np.sum(diagonal_distribution_right))
+    if np.sum(diagonal_distribution_left) >= np.sum(diagonal_distribution_right):
         return True  # левый верхний угол
     else:
         return False  # правый верхний угол
@@ -95,10 +69,10 @@ def diagonal_max_mean_definition(
 
 
 def sling_diagonal_definition(image: typing.Union[np.ndarray, list], *args, **kwargs):
-    # image_x_derivative = cv.Scharr(image, cv.CV_64F, 1, 0)
-    # image_y_derivative = cv.Scharr(image, cv.CV_64F, 0, 1)
-    temp_image = cv.GaussianBlur(image, (5, 5), 5)
-    image_edges = cv.Laplacian(temp_image, cv.CV_64F)
+    temp_image = image
+    image_x_derivative = cv.Scharr(temp_image, cv.CV_64F, 1, 0)
+    image_y_derivative = cv.Scharr(temp_image, cv.CV_64F, 0, 1)
+    image_edges = np.sqrt(image_x_derivative**2+image_y_derivative**2)
     angle = diagonal_max_mean_definition(image_edges, *args, **kwargs)
     return angle
 
