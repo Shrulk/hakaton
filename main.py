8a9fa657689f60c68c61c7a46d4fe7d108202140
Shrulk <58909805+Shrulk@users.noreply.github.com>
2024-04-25 00:23:00 +0300

    tmp

--- main.py
+++ main.py
@@ -12,6 +12,94 @@ from calculus.angles import Line, angles
 from cv_module.line_detection import sling_diagonal_definition_from_file
 
 
+def add_text_to_image(
+    image_rgb: np.ndarray,
+    label: str,
+    top_left_xy: tuple = (0, 0),
+    font_scale: float = 1,
+    font_thickness: float = 1,
+    font_face=cv2.FONT_HERSHEY_SIMPLEX,
+    font_color_rgb: tuple = (0, 0, 255),
+    bg_color_rgb: tuple | None = None,
+    outline_color_rgb: tuple | None = None,
+    line_spacing: float = 1,
+):
+    """
+    Adds text (including multi line text) to images.
+    You can also control background color, outline color, and line spacing.
+
+    outline color and line spacing adopted from: https://gist.github.com/EricCousineau-TRI/596f04c83da9b82d0389d3ea1d782592
+    """
+    OUTLINE_FONT_THICKNESS = 3 * font_thickness
+
+    im_h, im_w = image_rgb.shape[:2]
+
+    for line in label.splitlines():
+        x, y = top_left_xy
+
+        # ====== get text size
+        if outline_color_rgb is None:
+            get_text_size_font_thickness = font_thickness
+        else:
+            get_text_size_font_thickness = OUTLINE_FONT_THICKNESS
+
+        (line_width, line_height_no_baseline), baseline = cv2.getTextSize(
+            line,
+            font_face,
+            font_scale,
+            get_text_size_font_thickness,
+        )
+        line_height = line_height_no_baseline + baseline
+
+        if bg_color_rgb is not None and line:
+            # === get actual mask sizes with regard to image crop
+            if im_h - (y + line_height) <= 0:
+                sz_h = max(im_h - y, 0)
+            else:
+                sz_h = line_height
+
+            if im_w - (x + line_width) <= 0:
+                sz_w = max(im_w - x, 0)
+            else:
+                sz_w = line_width
+
+            # ==== add mask to image
+            if sz_h > 0 and sz_w > 0:
+                bg_mask = np.zeros((sz_h, sz_w, 3), np.uint8)
+                bg_mask[:, :] = np.array(bg_color_rgb)
+                image_rgb[
+                    y : y + sz_h,
+                    x : x + sz_w,
+                ] = bg_mask
+
+        # === add outline text to image
+        if outline_color_rgb is not None:
+            image_rgb = cv2.putText(
+                image_rgb,
+                line,
+                (x, y + line_height_no_baseline),  # putText start bottom-left
+                font_face,
+                font_scale,
+                outline_color_rgb,
+                OUTLINE_FONT_THICKNESS,
+                cv2.LINE_AA,
+            )
+        # === add text to image
+        image_rgb = cv2.putText(
+            image_rgb,
+            line,
+            (x, y + line_height_no_baseline),  # putText start bottom-left
+            font_face,
+            font_scale,
+            font_color_rgb,
+            font_thickness,
+            cv2.LINE_AA,
+        )
+        top_left_xy = (x, y + int(line_height * line_spacing))
+
+    return image_rgb
+
+
 CLASSES = ["background", "sling", "box"]
 detection_threshold = 0.9
 
@@ -23,9 +111,36 @@ model.load_state_dict(torch.load("outputs/model_150_END.pth", map_location=devic
 model.eval()
 
 
+def formate_elem(x):
+    x = str(round(x))
+    if "-" in x:
+        return "  " + x.ljust(5)
+    else:
+        return "   " + x.ljust(5)
+
+
 def gen_table(angles):
-    return np.array2string(angles, separator="\t", precision=2)[1:-1].replace(
-        "\n ", "\n"
+    x, y = angles.shape
+    if x < 4:
+        angles = np.append(angles, np.zeros((4 - x, y)), axis=0)
+    x, y = angles.shape
+    if y < 4:
+        angles = np.append(angles, np.zeros((x, 4 - y)), axis=1)
+    angles = np.vstack([[1, 2, 3, 4], angles])
+    angles = np.hstack([[[0], [1], [2], [3], [4]], angles])
+    return (
+        np.array2string(
+            angles,
+            separator="|",
+            precision=0,
+            formatter={"float_kind": lambda x: formate_elem(x)},
+        )
+        .replace("\n ", "\n")
+        .replace("[", "")
+        .replace("]", "")
+        .replace(" 0 ", " - ")
+        .replace(" - ", " # ", 1)
+        + "|"
     )
 
 
@@ -116,7 +231,7 @@ def process_video():
                             p1 = (int(box[2]), int(box[1]))
                             p2 = (int(box[0]), int(box[3]))
                         slings.append(Line(p1[0], p1[1], p2[0], p2[1]))
-                        table = gen_table(angles(slings))
+                        table = gen_table(angles(slings, "grad"))
                     for sling in slings:
                         cv2.line(
                             orig_image,
@@ -125,15 +240,16 @@ def process_video():
                             (0, 255, 0),
                             3,
                         )
-                    cv2.putText(
+                    add_text_to_image(
                         orig_image,
                         table,
-                        (500, 500),
+                        (600, 600),
+                        0.6,
+                        1,
                         cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                        (255, 0, 255),
-                        1,
-                        lineType=cv2.LINE_AA,
+                        (0, 255, 0),
+                        (30, 30, 30),
+                        line_spacing=1.05,
                     )
             # Рисуем рамку
             out.write(orig_image)
